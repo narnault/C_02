d9a4f0316f856a547b15b8ec9e160e461eb32d74
narnault <95000633+narnault@users.noreply.github.com>
2022-10-27 20:51:39 +0200

    Update ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -4,12 +4,9 @@ void	*ft_strcpy(char *dest, char *src)
 {
 	int	i;
 
-	i = 0;
-	while (src[i] != '\0')
-	{
+	i = -1;
+	while (src[++i])
 		dest[i] = src[i];
-		i++;
-	}
 	dest[i] = '\0';
 	return (dest);
 }
