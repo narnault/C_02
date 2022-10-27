f145b5e6beadfefb46ecf19b59eafcbac875670f
narnault <95000633+narnault@users.noreply.github.com>
2022-10-27 21:02:49 +0200

    Update ft_str_is_numeric.c

--- ft_str_is_numeric.c
+++ ft_str_is_numeric.c
@@ -3,21 +3,16 @@
 int     ft_str_is_numeric(char *str)
 {
     int i;
-    int j;
     
-    i = 0;
-    j = 0;
-    while (str[i])
-    {
-	(!(str[i] >= '0' && str[i] <= '9')     
+    i = -1;
+    while (str[++i])
+	    if(!(str[i] >= '0' && str[i] <= '9'))     
     		return (0);
-	i++;
-    }
     return (1);
 }
 
 int main()
 {
-    char    *str = "Salut";
+    char    *str = "434573";
     printf ("%i", ft_str_is_numeric(str));
 }
