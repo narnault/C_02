b4116d067ec7d62fa1f0405f2430091032656836
narnault <95000633+narnault@users.noreply.github.com>
2022-10-27 21:06:59 +0200

    Update ft_str_is_uppercase.c

--- ft_str_is_uppercase.c
+++ ft_str_is_uppercase.c
@@ -4,13 +4,10 @@ int     ft_str_is_uppercase(char *str)
 {
     int i;
     
-    i = 0;
-    while (str[i] != '\0')
-    {
+    i = -1;
+    while (str[++i])
         if (!(str[i] >= 'A' && str[i] <= 'Z'))
-		return (0);
-	i++;
-    }
+		    return (0);
     return (1);
 }
 
@@ -19,4 +16,3 @@ int main()
 	char *str = "SDLJ";
     	printf("%i", ft_str_is_uppercase(str));
 }
-
