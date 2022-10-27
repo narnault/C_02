33bd4f28ef3885c782e9ab85d9deb017936ef182
narnault <95000633+narnault@users.noreply.github.com>
2022-10-27 21:15:03 +0200

    Update ft_strupcase.c

--- ft_strupcase.c
+++ ft_strupcase.c
@@ -4,15 +4,10 @@ char	*ft_strupcase(char *str)
 {
 	int	i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
+	i = -1;
+	while (str[++i])
 		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] -= 32;
-		}
-		i++;
-	}
+		    str[i] -= 32;
 	return (str);
 }
 
