1227f3046413edd41dff2f56ee0e9d8dc5a159c4
narnault <95000633+narnault@users.noreply.github.com>
2022-10-27 21:22:11 +0200

    Update ft_strcapitalize.c

--- ft_strcapitalize.c
+++ ft_strcapitalize.c
@@ -2,27 +2,20 @@
 
 char    *ft_strcapitalize(char *str)
 {
-    {
-        int     i;
+    int     i;
 
-        i = 0;
-	while (str[i])
-        {
-                if (!((str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
+    i = -1;
+	while (str[++i])
+        if (!((str[i - 1] >= 'a' && str[i - 1] <= 'z') || 
                 (str[i - 1] >= 'A' && str[i - 1] <= 'Z') || 
-                (str[i - 1] >= '0' && str[i - 1] <= '9')))
-                {
+                    (str[i - 1] >= '0' && str[i - 1] <= '9')))
                         if (str[i] >= 'a' && str[i] <= 'z')
                             str[i] -= 32;
-                }
-                i++;
-        }
-        return (str);
-    }
+    return (str);
 }
 
 int main()
 {
         char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-        printf("%i", ft_strcapitalize(str));
+        printf("%s", ft_strcapitalize(str));
 }
