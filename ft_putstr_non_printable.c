0782dbe69d4e2d669ba1f102ec23cbd098d9b84b
narnault <95000633+narnault@users.noreply.github.com>
2022-10-27 21:35:07 +0200

    Update ft_putstr_non_printable.c

--- ft_putstr_non_printable.c
+++ ft_putstr_non_printable.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-void	hexa(int i) {
+void	hex(int i) {
 
 	char	*str;
 
@@ -9,27 +9,23 @@ void	hexa(int i) {
 	write(1, &str[i % 16], 1);
 }
 
-void	ft_putstr_non_printable(char *str) {
-
+void	ft_putstr_non_printable(char *str) 
+{
 	int		i;
 
-	i = 0;
-	while (str[i])
-	{
+	i = -1;
+	while (str[++i])
 		if (str[i] >= 0 && str[i] <= 31)
 		{
 			write(1, "\\", 1);
 			hex(str[i]);
-		}
+        	}
 		else
-		{
 			write(1, &str[i], 1);
-		}
-		i++;
-	}
 }
 
-int main() {
+int main() 
+{
     
     ft_putstr_non_printable("dasf sgedfa \n \t \f zh");
 }
