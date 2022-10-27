487b15f429b6376e202cc75653fe209c46413a73
narnault <95000633+narnault@users.noreply.github.com>
2022-10-27 21:00:28 +0200

    Update ft_str_is_alpha.c

--- ft_str_is_alpha.c
+++ ft_str_is_alpha.c
@@ -1,23 +1,18 @@
-#include <unistd.h>
+#include <stdio.h>
 
 int     ft_str_is_alpha(char *str)
 {
     int i;
-    int j;
     
-    i = 0;
-    j = 0;
-    while (str[i])
-    {
-        if (!(str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+    i = -1;
+    while (str[++i])
+        if (!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')))
         	return (0);
-        i++;
-    }
     return (1);
 }
 
 int main()
 {
     char    *str = "Salut";
-    ft_str_is_alpha(str);
+    printf("%i", ft_str_is_alpha(str));
 }
